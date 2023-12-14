a8a225d4e2689d4d22b24ad718053fc9273328cf
Jiri Vlasak <jirivlasak9@gmail.com>
2023-12-14 11:12:02 +0100

    Added python scripts

---
+++ scripts/data.py
@@ -0,0 +1,236 @@
+
+# %%
+import os
+import numpy as np
+import json
+import shutil
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import cv2
+
+# %%
+PATH = "C:/Users/tlust/Downloads/mtsd"
+
+splits_path = os.path.join(PATH, "splits")
+images_path = os.path.join(PATH, "images")
+annotations_path = os.path.join(PATH, "annotations")
+
+detect_path = os.path.join(PATH, "yolov8", "detect")
+cls_path = os.path.join(PATH, "yolov8", "classify")
+coco_path = os.path.join(PATH, "coco")
+
+# %% [markdown]
+# # YOLOv8
+
+# %%
+DOUBLE_STEP = False
+
+# %% [markdown]
+# ## Detection dataset
+# For a single-step process, classify directly into the full taxonomy; if not, employ binary detection.
+
+# %%
+labels = []
+
+# statistics
+rejected = 0
+total = 0
+sign_distr = {}
+
+for split in ['train', 'val']:
+    print("Processing {} split...".format(split))
+
+    # 0. create output directories if not exists
+    out_dir = os.path.join(detect_path, split)
+    if not os.path.exists(out_dir):
+        os.makedirs(os.path.join(out_dir, "images"))
+        os.makedirs(os.path.join(out_dir, "labels"))
+
+    with open(os.path.join(splits_path, split + ".txt")) as f:
+        ids = f.readlines()
+
+    for id in tqdm(ids, total=len(ids)):
+        total += 1    
+
+        # 1. set and validate paths
+        id = id.strip()
+        img_path = os.path.join(images_path, f"{id}.jpg")
+        ann_path = os.path.join(annotations_path, f"{id}.json")
+        out_img_path = os.path.join(out_dir, "images", f"{id}.jpg")
+        out_ann_path = os.path.join(out_dir, "labels", f"{id}.txt")  
+
+        # 1.2. skip if image or annotation does not exists
+        if (not os.path.exists(img_path)) or (not os.path.exists(ann_path)):
+            rejected += 1
+            continue
+
+        # 2. read annotation 
+        with open(ann_path, 'r') as f:
+            ann = json.load(f)
+
+        # 2.1 skip if panormaic image
+        if ann['ispano'] == True:
+            rejected += 1
+            continue
+
+        # 3. create annotation file and copy image
+        is_empty = True
+        for obj in ann['objects']:
+            # 3.1 get label index
+            if DOUBLE_STEP:
+                obj['label'] = 'traffic-sign'                    
+            else:
+                if obj['label'] == 'other-sign':
+                    continue
+                else:
+                    obj['label'] = '--'.join(obj['label'].split('--')[1:-1])                      
+                
+            if obj['label'] not in labels:
+                labels.append(obj['label'])
+            label = labels.index(obj['label'])
+
+            # 3.2 set sign distribution
+            sign_distr[label] = sign_distr[label] + 1 if label in sign_distr else 1
+
+            # 3.3 get bounding box
+            bbox = obj['bbox']
+            x_center = np.clip(((bbox['xmin'] + bbox['xmax']) / 2) / ann['width'], 0, 1)
+            y_center = np.clip(((bbox['ymin'] + bbox['ymax'] ) / 2) / ann['height'], 0, 1)
+            width = np.clip((bbox['xmax'] - bbox['xmin']) / ann['width'], 0, 1)
+            height = np.clip((bbox['ymax'] - bbox['ymin']) / ann['height'], 0, 1)
+            obj_ann = f"{label} {x_center} {y_center} {width} {height} \n"
+
+            # 3.4 write annotation
+            if width > 0.01 and height > 0.01:
+                is_empty = False
+                with open(out_ann_path, "a") as f:  
+                    f.write(obj_ann)
+            
+            if is_empty == False:
+                # 3.5. copy the image
+                shutil.copy(img_path, out_img_path)
+            else:
+                rejected += 1
+
+# 4. create dataset.yaml
+with open(os.path.join(detect_path, "dataset.yaml"), "a") as f:
+    f.write("path: {detect_path}\n")
+    f.write(f"train: {os.path.join('train', 'images')}\n")
+    f.write(f"val: {os.path.join('val', 'images')}\n")
+    f.write(f"names:\n")
+    for ix, label in enumerate(labels):
+        f.write(f"  {ix}: {label}\n")
+
+# %% [markdown]
+# ## Classification dataset
+# If a two-stage pipeline is chosen, then construct a classification dataset that exclusively includes extracted signs, sorted by their respective labels.
+
+# %%
+if DOUBLE_STEP:
+    labels = []
+
+    # statistics
+    rejected = 0
+    total = 0
+    sign_distr = {}
+
+
+    for split in ['train', 'val', 'test']:
+        print("Processing {} split...".format(split))
+   
+        with open(os.path.join(splits_path, split + ".txt")) as f:
+            ids = f.readlines()
+
+        for id in tqdm(ids, total=len(ids)):
+            total += 1
+
+            # 1. set and validate paths
+            id = id.strip()
+            img_path = os.path.join(images_path, f"{id}.jpg")
+            ann_path = os.path.join(annotations_path, f"{id}.json")
+    
+            # 1.2. skip if image or annotation does not exists
+            if (not os.path.exists(img_path)) or (not os.path.exists(ann_path)):
+                rejected += 1
+                continue
+
+            # 2. load the image
+            img = cv2.imread(img_path)
+
+            # 3. extract traffic sign and create classification dataset                
+            with open(ann_path, 'r') as f:
+                ann = json.load(f)
+
+            for obj in ann['objects']:
+                # 3.1 skip if other-sign
+                if obj['label'] == 'other-sign':
+                    continue
+                # remove --gN suffix
+                obj['label'] = '--'.join(obj['label'].split('--')[:-1])
+
+                # 3.2 get sign path and create directory if not exists
+                sign_dir = os.path.join(cls_path, split, obj['label'])
+                if not os.path.exists(sign_dir):
+                    os.makedirs(sign_dir)
+
+                # 3.3 increment sign counter
+                if obj['label'] not in labels:
+                    labels.append(obj['label'])
+                label = labels.index(obj['label'])            
+                sign_distr[label] = sign_distr[label] + 1 if label in sign_distr else 1
+
+                # 3.4 get bounding box
+                sign = img[
+                    int(obj['bbox']['ymin']):int(obj['bbox']['ymax']), 
+                    int(obj['bbox']['xmin']):int(obj['bbox']['xmax'])
+                ]
+
+                # 3.4 save sign
+                if sign.shape[0] > 0 and sign.shape[1] > 0 and sign.shape[2] > 0:
+                    sign_path = os.path.join(sign_dir, f"{id}_{obj['key']}.jpg")
+                    cv2.imwrite(sign_path, sign)
+
+# %% [markdown]
+# ## 2. view dataset statistics
+
+# %%
+print("Images - total: {}".format(total))
+print("Images - rejected: {}".format(rejected) + " ({:.2f}%)".format(rejected / total * 100))
+print("Signs: {}".format(np.sum(sign_distr.values())))
+for ix, value in sign_distr.items():
+    print(f"{labels[ix]}: {value}")
+plt.bar(labels, sign_distr.values())
+
+# %% [markdown]
+# # COCO
+
+
+# %%
+from globox import AnnotationSet
+
+# %%
+# create dirs if not exists
+img_dir = os.path.join(coco_path, "images")
+ann_dir = os.path.join(coco_path, "annotations")
+if not os.path.exists(ann_dir) or not os.path.exists(img_dir):
+    os.makedirs(ann_dir)
+    os.makedirs(img_dir)
+
+for split in ['train', 'val']:
+    yolo_img_dir = os.path.join(detect_path, split, "images")
+
+    # copy images
+    for img_name in os.listdir(yolo_img_dir):
+        shutil.copy(
+            os.path.join(yolo_img_dir, img_name),
+            os.path.join(img_dir, img_name)
+        )
+
+    # convert annotations
+    yolo = AnnotationSet.from_yolo_v5(
+        folder=os.path.join(detect_path, split, "labels"),
+        image_folder=yolo_img_dir
+    )
+    yolo.save_coco(os.path.join(ann_dir, split + ".json"), auto_ids=True)
+
+
---
+++ scripts/model.py
@@ -0,0 +1,258 @@
+# %%
+TRAIN_YOLO = False
+TRAIN_DETR = True
+
+# %% [markdown]
+# # YOLOv8 training
+# This part of the project explores capabilites of the YOLOv8 model.
+
+# %%
+DOUBLE_STEP = False
+BASE_PATH = "C:/Users/tlust/Downloads/mtsd/yolov8"
+
+# %%
+
+# %%
+import os
+from ultralytics import YOLO
+
+# %%
+detect_path = os.path.join(BASE_PATH, "detect", "dataset.yaml")
+classify_path = os.path.join(BASE_PATH, "classify")
+
+# %% [markdown]
+# ## One-step fully taxonomy detection + classification to 313 classes
+# Initial experiment to assess the model's default performance across the entire taxonomy. Anticipated to yield suboptimal results due to the extensive number of classes.
+
+# %%
+if TRAIN_YOLO and DOUBLE_STEP == False:
+    model = YOLO("yolov8m.pt")  # load a pretrained model
+    results = model.train(data=detect_path, epochs=100, imgsz=640, batch=16, fliplr=0)
+    print(results)
+
+# %% [markdown]
+# ## 2-stage pipeline
+# Anticipated to yield improved outcomes as a result of decoupling.
+
+# %% [markdown]
+# ### 1. train binary sign detector
+
+# %%
+if TRAIN_YOLO and DOUBLE_STEP:
+    model = YOLO('yolov8n.pt')  # load a pretrained model
+    results = model.train(data=detect_path, epochs=10, imgsz=640)
+    print(results)
+
+# %% [markdown]
+# ### 2. train sign classifier
+
+# %%
+if TRAIN_YOLO and DOUBLE_STEP:
+    model = YOLO('yolov8x-cls.pt')  # load a pretrained model
+    results = model.train(data=classify_path, epochs=100, imgsz=224, batch=128)
+    print(results)
+
+# %% [markdown]
+# # DETR
+
+# %%
+PATH = 'C:/Users/tlust/Downloads/mtsd/coco'
+NUM_CLASSES = 235
+BATCH_SIZE = 6
+
+# %%
+import os
+import torch
+import torchvision
+from transformers import DetrImageProcessor
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from transformers import DetrForObjectDetection
+from pytorch_lightning import Trainer
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+
+# %%
+class MappilaryDataset(torchvision.datasets.CocoDetection):
+    def __init__(self, dir, processor, train=True):
+        super(MappilaryDataset, self).__init__(
+            os.path.join(dir, "images"),
+            os.path.join(dir, "annotations", "train.json" if train else "val.json")
+        )
+        self.processor = processor
+
+    def __getitem__(self, idx):
+        img, target = super(MappilaryDataset, self).__getitem__(idx)
+        image_id = self.ids[idx]
+        target = {
+            'image_id': image_id, 
+            'annotations': target
+        }
+        encoding = self.processor(
+            images=img, 
+            annotations=target, 
+            return_tensors="pt"
+        )
+        pixel_values = encoding["pixel_values"].squeeze()
+        target = encoding["labels"][0]
+        return pixel_values, target
+    
+    def collate_fn(self, batch):
+        pixel_values = [item[0] for item in batch]
+        encoding = self.processor.pad(pixel_values, return_tensors="pt")
+        labels = [item[1] for item in batch]
+        batch = {}
+        batch['pixel_values'] = encoding['pixel_values']
+        batch['pixel_mask'] = encoding['pixel_mask']
+        batch['labels'] = labels
+        return batch  
+
+
+# %%
+class Detr(pl.LightningModule):
+   def __init__(self, lr, lr_backbone, weight_decay, train_dl, val_dl):
+      super().__init__()
+      self.model = DetrForObjectDetection.from_pretrained(
+         "facebook/detr-resnet-50",
+         num_labels=NUM_CLASSES,
+         ignore_mismatched_sizes=True
+      )
+      self.lr = lr
+      self.lr_backbone = lr_backbone
+      self.weight_decay = weight_decay
+      self.train_dl = train_dl
+      self.val_dl = val_dl
+      self.map = MeanAveragePrecision()
+
+   def forward(self, pixel_values, pixel_mask):
+      outputs = self.model(
+         pixel_values=pixel_values, 
+         pixel_mask=pixel_mask
+      )
+      return outputs   
+   
+   def training_step(self, batch, batch_idx):
+      # 1: forward pass
+      outputs = self.model(
+         pixel_values=batch["pixel_values"], 
+         pixel_mask=batch["pixel_mask"],
+         labels=[
+            {k: v.to(self.device) for k, v in t.items()} 
+            for t in batch["labels"]
+         ]
+      )
+
+      # 2: log loss
+      self.log("train_loss", outputs.loss)
+      for k,v in outputs.loss_dict.items():
+         self.log("train_" + k, v.item())
+
+      # 3: backpropagation
+      return outputs.loss
+
+   def validation_step(self, batch, batch_idx):
+      # 1: forward pass
+      outputs = self.model(
+         pixel_values=batch["pixel_values"], 
+         pixel_mask=batch["pixel_mask"],
+         labels=[
+            {k: v.to(self.device) for k, v in t.items()} 
+            for t in batch["labels"]
+         ]
+      )
+
+      # 2: log loss
+      self.log("val_loss", outputs.loss)
+      for k,v in outputs.loss_dict.items():
+         self.log("val_" + k, v.item())
+
+      # 3: compute mAP
+      preds = [
+         {
+            "boxes": outputs["pred_boxes"][i],
+            "scores": torch.softmax(outputs["logits"][i], -1).max(-1).values,
+            "labels": torch.softmax(outputs["logits"][i], -1).max(-1).indices
+         } for i in range(len(outputs["pred_boxes"]))
+      ]
+      targets = [
+         {
+            "boxes": label["boxes"],
+            "labels": label["class_labels"]
+         } 
+         for label in batch["labels"]
+      ]
+      self.map.update(preds, targets)
+   
+   def on_validation_epoch_end(self):
+      self.log('val_mAP',self.map.compute()['map'], prog_bar=True)
+
+   def configure_optimizers(self):
+      param_dicts = [
+            {
+               "params": [p for n, p in self.named_parameters() if "backbone" not in n and p.requires_grad]
+            },
+            {
+               "params": [p for n, p in self.named_parameters() if "backbone" in n and p.requires_grad],
+               "lr": self.lr_backbone,
+            }
+      ]
+      optimizer = torch.optim.AdamW(
+         param_dicts, 
+         lr=self.lr,
+         weight_decay=self.weight_decay
+      )
+      return optimizer 
+   
+   def train_dataloader(self):
+      return self.train_dl
+   
+   def val_dataloader(self):
+      return self.val_dl
+
+# %%
+if TRAIN_DETR:
+  processor = DetrImageProcessor.from_pretrained(
+      "facebook/detr-resnet-50"
+  )
+
+  # 1: load dataset
+  train_dataset = MappilaryDataset(
+      dir=PATH, 
+      processor=processor
+  )
+  val_dataset = MappilaryDataset(
+      dir=PATH, 
+      processor=processor, train=False
+  )
+  train_dataloader = DataLoader(
+    train_dataset, 
+    collate_fn=train_dataset.collate_fn, 
+    batch_size=BATCH_SIZE, 
+    shuffle=True
+  )
+  val_dataloader = DataLoader(
+    val_dataset, 
+    collate_fn=train_dataset.collate_fn, 
+    batch_size=BATCH_SIZE
+  )
+
+  # 2: init model
+  model = Detr(
+    lr=1e-4, 
+    lr_backbone=1e-5,
+    weight_decay=1e-4, 
+    train_dl=train_dataloader, 
+    val_dl=val_dataloader
+  )
+
+  # 3: fine-tune
+  trainer = Trainer(
+      devices=1, 
+      accelerator="gpu",
+      max_epochs=10, 
+      gradient_clip_val=0.1, 
+      accumulate_grad_batches=(32 // BATCH_SIZE), 
+      log_every_n_steps=1
+  )
+  trainer.fit(model)
+
+
